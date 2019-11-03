c6810dd430330a1f5457a500d3e383a00a339788
Matt Spraggs <matthew.spraggs@gmail.com>
2019-11-03 17:39:22 +0000

    Tasks: Add task to deposit to account each month

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 import dramatiq
@@ -27,6 +28,29 @@ def _get_credentials(client=None):
         k: client.get(k).decode()
         for k in ('client_id', 'client_secret', 'refresh_token', 'access_token')
     }
+
+
+def _count_business_days_between_dates(start, finish):
+    count = 0
+    current = start
+
+    while current != finish:
+        if current.weekday() < 5:
+            count += 1
+        current += datetime.timedelta(days=1)
+
+    return count
+
+
+def _add_months(start, num_months):
+    num_years = (start.month + num_months - 1) // 12
+    new_month = (start.month + num_months - 1) % 12 + 1
+    # TODO: Handle day of month out of bounds for month
+    return datetime.date(
+        year=start.year + num_years,
+        month=new_month,
+        day=start.day,
+    )
 
 
 @cron('0 */6 * * *')  # Run every six hours
@@ -62,3 +86,26 @@ def withdraw_from_pot():
     monzo_client.withdraw_from_pot(account_id, pot_id, int(amount))
 
     logger.info('Withdrew money from pot.')
+
+
+@cron('30 18 26 * *')
+@dramatiq.actor
+def deposit_to_pot():
+    redis_client = redis.Redis(host='redis')
+
+    today = datetime.today()
+    month_ahead = _add_months(today, 1)
+    num_days = _count_business_days_between_dates(today, month_ahead)
+
+    account_id = redis_client.get('account_id').decode()
+    pot_id = redis_client.get('pot_id').decode()
+    day_amount = redis_client.get('amount').decode()
+
+    month_amount = int(day_amount) * num_days
+
+    credentials = _get_credentials(redis_client)
+
+    monzo_client = monzo.MonzoClient(**credentials)
+    monzo_client.deposit_to_pot(pot_id, account_id, month_amount)
+
+    logger.info(f'Deposited {month_amount} to pot.')
